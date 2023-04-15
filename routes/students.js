503c5121b098b83d8a3e307659701a7c27cdf80c
Saurabh Panja <saurabh.panja@phamax.ch>
2023-04-15 13:13:05 +0530

    checkpoint

--- routes/students.js
+++ routes/students.js
@@ -4,7 +4,7 @@ var router = express.Router();
 const ensureAuthenticated = require("../middleware/authenticate");
 
 router.get("/", ensureAuthenticated, function (req, res, next) {
-    res.render("students/newStudent", { title: "Students" });
+    res.render("students/newStudent", { title: "Students", success: req.flash("success"), error: req.flash("error") });
 });
 
 module.exports = router;
